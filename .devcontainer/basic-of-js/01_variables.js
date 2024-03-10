55cb6a2725519aa062a00223ae93cbe8844d54b8
krishndev <sawkrishndev983@gmail.com>
2024-03-10 21:19:45 +0530

    changes

---
+++ .devcontainer/basic-of-js/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "ramu@google.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+
+// accountId = 2
+accountEmail = "hc@hc.com"
+accountPassword = "6538"
+accountCity = "Kolkata"
+console.table([accountId,accountEmail,accountPassword,accountCity])
