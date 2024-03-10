2e29792b21a127c0e303a7981576c807b90ad023
krishndev <sawkrishndev983@gmail.com>
2024-03-10 09:17:02 +0530

    conversionOfDatatypes

---
+++ .devcontainer/basic-of-js/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict";
+// let stateId;
+// console.log(stateId);
+// let stateId = "";
+let myVariable = "OriginalValue";
+console.log(myVariable); // This will output 'OriginalValue'
+
+// Changing the value of myVariable
+ myVariable = "NewValue";
+console.log(myVariable); // This will output 'NewValue'
---
+++ .devcontainer/basic-of-js/conversionOperator.js
@@ -0,0 +1,47 @@
+// let score = 33
+// score = "33abc"
+// console.log(typeof score)
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+                   //For null
+
+// let score = 33
+// score = null
+// console.log(typeof score)
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+                //For undefiend
+
+// let score = undefined
+// //score = "33abc"
+// console.log(typeof score)
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+              // for boolean 
+
+              let isLoggedin = 0
+              let booleanIsLoggedIn = Boolean(isLoggedin)
+              console.log(booleanIsLoggedIn)
+    
+/* "33" => 33
+    "33abc" => NaN
+    true => 1; false => 0
+
+    1 => true ; 0 => false
+    "" => false
+    "hitesh" => true */
+
+    let someNumber = 33
+    let stringNumber = String(someNumber)
+    console.log(stringNumber)
+    console.log(typeof stringNumber);
