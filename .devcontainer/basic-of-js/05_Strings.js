3629d8c4ed622ec05a5febdea099c30aa086f688
krishndev <sawkrishndev983@gmail.com>
2024-03-13 20:06:48 +0530

    changes in 06 and 05

--- .devcontainer/basic-of-js/05_Strings.js
+++ .devcontainer/basic-of-js/05_Strings.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`hello my name is ${name} and my repo Count is ${repoCount}`);
 
-const gameName = new String('hiteshhc')
+const gameName = new String('hitesh-hc-com')
 console.log(gameName);
 console.log(gameName[0]);
 console.log(gameName.__proto__);
@@ -21,12 +21,23 @@ console.log(gameName.indexOf('t'));
 const newString = gameName.substring(0,4)
 console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-8, 6)
 console.log(anotherString);
 
 const anotherid1 = "         hikes      "
 console.log(anotherid1);
 console.log(anotherid1.trim());
+
+
+let url = "https://hike.com/hike%20choudhary"
+console.log(url.replace('%20', '_'));
+console.log (url.includes('hike'))
+console.log (url.includes('himesh'))
+
+console.log(gameName.split('-')); // Based on separators you can separate in an array;
+
+
+
 
 
 
---
+++ .devcontainer/basic-of-js/06_nums_and_math.js
@@ -0,0 +1,33 @@
+// const nums = 400;
+// console.log(nums);
+
+// const anotherNum = new Number(600)
+// console.log(anotherNum)
+
+// console.log(anotherNum.toString().length);
+// console.log(anotherNum.toFixed(2));
+
+// const newNumber = 132.874673
+// console.log(newNumber.toPrecision(4));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//*********************************** Math **********
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,4,8,7,6));
+// console.log(Math.max(7,9,5,4,6));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max -min +1))+min);
